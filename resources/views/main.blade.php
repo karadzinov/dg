61b81a772ec0109971786a7d7c8ad3417ab7b628
martin <martin@pingdevs.com>
2024-01-09 17:44:35 +0100

    hexa

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -50,48 +50,85 @@
             <div class="gallery">
                 <div>
 
-                    <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/blogs/10496/images/a67164-f1f1-82e5-e8c-afffdbd7a060_AdobeStock_110110063.jpeg" alt="a house on a mountain">
+                    <img
+                        src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/blogs/10496/images/a67164-f1f1-82e5-e8c-afffdbd7a060_AdobeStock_110110063.jpeg"
+                        alt="a house on a mountain">
                 </div>
 
                 <div class="active" id="invitations">
                     <p class="hex-title">Онлајн Покани</p>
-                    <img src="https://www.brides.com/thmb/GOB3oUZwX5epKWGUUwBim6d48Bc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/diy-wedding-invitations-getty-recirc-1123-85cf65e909bb426d951a250e4134d88e.jpg" alt="sime pink flowers">
+                    <img
+                        src="https://www.brides.com/thmb/GOB3oUZwX5epKWGUUwBim6d48Bc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/diy-wedding-invitations-getty-recirc-1123-85cf65e909bb426d951a250e4134d88e.jpg"
+                        alt="Online Invitations" data-link="invitation/create">
                 </div>
                 <div>
                     <p class="hex-title">Ресторани</p>
-                    <img src="https://images.squarespace-cdn.com/content/v1/62125959ab8da87f49fb31cb/1680231108755-OUAGK6JP1X3ACHLLY6D0/059_Wedding+at+The+Paseo+venue+in+Apache+Junction%2C+Arizona+only+45+minutes+from+Phoenix..jpg" alt="big rocks with some trees">
+                    <img
+                        src="https://images.squarespace-cdn.com/content/v1/62125959ab8da87f49fb31cb/1680231108755-OUAGK6JP1X3ACHLLY6D0/059_Wedding+at+The+Paseo+venue+in+Apache+Junction%2C+Arizona+only+45+minutes+from+Phoenix..jpg"
+                        alt="Restaurants" data-link="restaurants">
                 </div>
                 <div>
                     <p class="hex-title">Фотографи</p>
-                    <img src="https://photor.org/wp-content/uploads/2020/10/types-of-photographs-and-photographers-see-the-differences-between-one.jpg"
-                          alt="a waterfall, a lot of tree and a great view from the sky">
+                    <img
+                        src="https://photor.org/wp-content/uploads/2020/10/types-of-photographs-and-photographers-see-the-differences-between-one.jpg"
+                        alt="Photographers" data-link="photographers">
                 </div>
-                <div><img src="https://theweddingavenue.co.uk/wp-content/uploads/2022/10/gold-wedding-rings.jpeg" alt="a cool landscape"></div>
-                <div><img src="https://www.ripleys.com/wp-content/uploads/2018/06/shutterstock_117779707.jpg" alt="inside a town between two big buildings">
+                <div><img src="https://theweddingavenue.co.uk/wp-content/uploads/2022/10/gold-wedding-rings.jpeg"
+                          alt="Wedding rings">
+                </div>
+                <div><img src="https://www.ripleys.com/wp-content/uploads/2018/06/shutterstock_117779707.jpg"
+                          alt="Services">
                 </div>
                 <div>
                     <p class="hex-title">Музичари</p>
-                    <img src="https://i.ytimg.com/vi/ePUHdvzNOVs/hqdefault.jpg" alt="a great view of the sea above the mountain"></div>
+                    <img src="https://i.ytimg.com/vi/ePUHdvzNOVs/hqdefault.jpg"
+                         alt="Musicians" data-link="musicians"></div>
 
             </div>
 
 
         </div>
     </div>
-        @endsection
-        @section('scripts')
-            <script>
-                $(".hexa div").on("mouseout", function () {
-                    $(this).removeClass("active");
-                    $("#invitations").addClass("active");
-                });
+@endsection
+@section('scripts')
+    <script>
+        $(".gallery div").on("mouseout", function () {
+            $(this).removeClass("active");
+            $("#invitations").addClass("active");
+
+        });
+
+        $(".gallery > div").on("click", function () {
 
 
-                $(".gallery div").on("mouseover", function () {
-                    $("#invitations").removeClass("active");
-                });
+           let img = $(this).children('img')[0];
+
+            window.location = '/' + $(img).data('link');
+        });
 
 
-            </script>
+        $(".gallery div").on("mouseover", function () {
+            $("#invitations").removeClass("active");
+
+        });
+
+
+        function check_from_top_de() { // Create our function
+            var scroll = $(window).scrollTop(); // Check scroll distance
+            if (scroll >= 700) { // If it is equal or more than 300 - you can change this to what you want
+                $("#invitations").parent().removeClass("gallery-gap");
+            } else {
+                $("#invitations").parent().addClass("gallery-gap");
+            }
+        }
+
+        check_from_top_de(); // On load, run the function
+
+        $(window).scroll(function () { // When scroll - run function
+            check_from_top_de();
+        });
+
+
+    </script>
 @endsection
 
