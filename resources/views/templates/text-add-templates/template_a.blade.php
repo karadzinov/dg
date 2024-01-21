626bb12dee72736095e0a966a42c89538c8a6c2d
martin <martin@pingdevs.com>
2024-01-21 13:36:37 +0100

    template analytics

--- resources/views/templates/text-add-templates/template_a.blade.php
+++ resources/views/templates/text-add-templates/template_a.blade.php
@@ -54,6 +54,14 @@
     <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TS8GHQQ2');</script>
+    <!-- End Google Tag Manager -->
+
     <style>
         #male_text_message {
             display: none;
@@ -720,6 +728,20 @@
             $("#female_text").attr('data-title', 'Кликнете за промена на текстот');
         }, 200);
     });
+</script>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TS8GHQQ2"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-H7XM5SM8DV"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-H7XM5SM8DV');
 </script>
 </body>
 </html>
