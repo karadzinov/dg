6ac8935150d3825a674b6790f9e6cf9c9335c3b3
martin <martin@pingdevs.com>
2024-01-26 11:42:46 +0100

    Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,33 +1,59 @@
 <footer class="bg-dark">
     <div class="container">
         <div class="row align-items-center pt-8 pb-4">
-            <div class="col-md mb-5 mb-md-0">
+            <div class="col-md mb-4 mb-md-0 text-center">
+                <a href="/">
                 <img src="/dist/images/logos/logo-main.png" class="dark-logo" width="100" alt="">
+                </a>
             </div>
-            <!-- End Col -->
 
-
-            <!-- End Col -->
-        </div>
-        <!-- End Row -->
-
-
-
-        <div class="row align-items-md-center py-6">
-            <div class="col-md mb-3 mb-md-0">
+            <div class="col-md mb-4 mb-md-0 text-center">
+                <p class="text-white-50 mt-2">Услуги</p>
                 <!-- List -->
-                <ul class="list-inline list-px-2 mb-0">
-                    <li class="list-inline-item"><a class="link link-light link-light-75" href="/privacy">Приватност</a></li>
-                    <li class="list-inline-item"><a class="link link-light link-light-75" href="/terms">Услови на користење</a></li>
+                <ul class=" list-px-2 mb-0">
+                    <li class="listing-item"><a class="link link-light link-light-75" href="/invitation/package"> <i class="ti ti-mail"></i> Покани</a></li>
+                    <li class="listing-item"><a class="link link-light link-light-75" href="/restaurants"><i class="ti ti-salad"></i> Ресторани</a></li>
 
-
+                    <li class="listing-item"><a class="link link-light link-light-75" href="/musicians"><i class="ti ti-music"></i> Музичари</a></li>
+                    <li class="listing-item"><a class="link link-light link-light-75" href="/photographers"><i class="ti ti-camera"></i>  Фотографи</a></li>
                 </ul>
                 <!-- End List -->
             </div>
             <!-- End Col -->
 
+            <div class="col-md mb-4 mb-md-0 text-center">
+                <p class="text-white-50">Заследете не</p>
+                <!-- List -->
+                <ul class=" list-px-2 mb-0">
+                    <li class="listing-item"><a class="link link-light link-light-75" href="https://facebook.com/dragigosti" target="_blank"> <i class="ti ti-brand-facebook"></i> Facebook</a></li>
+                    <li class="listing-item"><a class="link link-light link-light-75" href="https://instagram.com/dragigosti" target="_blank"> <i class="ti ti-brand-instagram"></i> Instagram</a></li>
+                    <li class="listing-item"><a class="link link-light link-light-75" href="https://twitter.com/dragigosti" target="_blank"> <i class="ti ti-brand-twitter"></i> Twitter</a></li>
+
+                </ul>
+                <!-- End List -->
+            </div>
+        </div>
+
+
+
+
+        <div class="row align-items-md-center py-6">
+
+
+            <div class="col-md mb-3 mb-md-0">
+                <!-- List -->
+                <ul class="list-inline list-px-2 mb-0">
+                    <li class="list-inline-item"><a class="link link-light link-light-75" href="/privacy">Приватност</a></li>
+                    <li class="list-inline-item"><a class="link link-light link-light-75" href="/terms">Услови на користење</a></li>
+                </ul>
+                <!-- End List -->
+            </div>
+            <!-- End Col -->
+
+
+
             <div class="col-md-auto">
-                <p class="fs-5 text-white-70 mb-0">© DragiGosti. {{ date('Y') }}</p>
+                <p class="fs-4 text-white mb-0">© DragiGosti {{ date('Y') }}</p>
             </div>
             <!-- End Col -->
         </div>
