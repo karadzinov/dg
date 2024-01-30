91ac962ef974f0c5ab78c5a8fc4f4b2d97c38190
martin <martin@pingdevs.com>
2024-01-30 21:53:21 +0100

    Copy invitation and changes

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -487,5 +487,28 @@
 
     gtag('config', 'G-H7XM5SM8DV');
 </script>
+
+
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '1339434706768448',
+            cookie     : true,
+            xfbml      : true,
+            version    : '2'
+        });
+
+        FB.AppEvents.logPageView();
+
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 </body>
 </html>
