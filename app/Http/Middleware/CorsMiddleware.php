b039f40a7821bd5abe6d550b62f19cc373eafa62
Martin Karadzinov <martin@pingdevs.com>
2025-03-18 15:30:59 +0100

    Adding cors

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CorsMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        // Allow requests from your frontend
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
+
+        return $response;
+    }
+}
