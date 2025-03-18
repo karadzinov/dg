a9521778d23efeab6e45a2296be8379f51689c1c
Martin Karadzinov <martin@pingdevs.com>
2025-03-18 15:09:07 +0100

    Building api - update user request

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UpdateUserRequest extends FormRequest
 {
@@ -19,5 +21,15 @@ class UpdateUserRequest extends FormRequest
             'category' => 'required|string',
             'role_id' => 'required|integer|exists:roles,id',
         ];
+    }
+
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'errors' => $validator->errors()
+            ], 422)
+        );
     }
 }
